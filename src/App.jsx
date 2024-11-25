9455d2bcf50ebe6feb0f3c70bf6c39ef9a114224
jooao7 <jvitorfjv781@gmail.com>
2024-11-25 03:14:26 -0300

    Corrige GlobalStyle.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
 import Novidades from "./Components/Novidades/Novidades";
 import Sobre from "./Components/Sobre/Sobre";
-import GlobalStyle from "../GlobalStyle";
+import GlobalStyle from "../GlobalStyle.jsx";
 
 export default function App() {
   return (
